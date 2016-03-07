214054a0f8a9bded41675bb031e331950234b1f1
jafetbjarkar <jafetbjarkar@gmail.com>
2016-03-07 15:45:09 +0000

    Initial code

---
+++ hicharts-initial-values.js
@@ -0,0 +1,17 @@
+{"id":"sale","name":"SALE_ADJUSTED","data":[],"color":"#F48618","type":"column","index":4,"legendIndex":4,"zIndex":5,"stacking":"normal","stack":1}
+{"id":"original_sale","name":"SALE_ORIGINAL","data":[],"color":"#964802","type":"line","index":1,"legendIndex":2,"zIndex":10,"marker":{"enabled":true,"symbol":"diamond","radius":5}}
+{"id":"stock_history","name":"STOCK","data":[],"color":"#49A3F8","fillOpacity":0.5,"type":"area","lineWidth":1,"index":6,"legendIndex":14,"zIndex":3,"step":true}
+{"id":"forecast","name":"SALE_FORECAST","data":[],"color":"#F1B925","type":"column","index":1,"legendIndex":6,"zIndex":4,"stack":1,"stacking":"normal","tooltip":{"valueDecimals":1}}
+{"id":"reserved","name":"SALE_RESERVED","data":[],"color":"#8ACB03","type":"column","index":6,"legendIndex":10,"zIndex":-10,"stacking":"normal","stack":2,"pointPlacement":0.08}
+{"id":"known_demand","name":"SALE_KNOWN_DEMAND","data":[],"color":"#608B02","type":"column","index":6,"legendIndex":12,"zIndex":-10,"stacking":"normal","stack":2,"pointPlacement":0.08}
+{"id":"estimated_stock","name":"ESTIMATED_STOCK","data":[],"color":"#61C3F8","type":"area","lineWidth":1,"fillOpacity":0.4,"index":7,"legendIndex":16,"zIndex":0,"step":true}
+{"id":"safety_stock_for_order_period","name":"SAFETY_STOCK_ORDER_PERIOD","data":[],"color":"#49A3F8","type":"line","lineWidth":2,"index":8,"legendIndex":18,"zIndex":8,"dashStyle":"dash","marker":{"enabled":false}}
+{"id":"undelivered","name":"UNDELIVERED","data":[],"color":"#255592","type":"column","pointWidth":3,"index":9,"legendIndex":20,"zIndex":30}
+{"id":"order_qty","name":"ORDER_SELECTED","data":[],"color":"#BA0B9F","type":"column","pointWidth":3,"index":10,"legendIndex":22,"zIndex":20}
+{"id":"purchase_plan_order_qty","name":"PURCHASE_PLAN_ORDER","data":[],"color":"#E753D1","type":"column","pointWidth":3,"index":11,"legendIndex":24,"zIndex":22}
+{"id":"bom_demand","name":"SALE_BOM_DEMAND","data":[],"color":"#A7F503","type":"column","index":5,"legendIndex":7,"zIndex":10,"stacking":"normal","stack":2,"pointPlacement":0.08}
+{"id":"planner_sales_plan","name":"SALES_PLAN_PLANNER","data":[],"color":"#F15225","type":"column","index":4,"legendIndex":6,"zIndex":10,"pointPlacement":0.08,"stacking":"normal","stack":2}
+{"id":"min_stock","name":"MINIMUM_STOCK_LEVEL","data":[],"color":"#552d86","type":"line","lineWidth":1,"index":8,"legendIndex":18,"zIndex":8,"marker":{"enabled":false}}
+
+
+'item_details_chart_highcharts_options', '{"chart":{"renderTo":"container","type":"column","zoomType":"x","spacingBottom":2,"panning":true,"panKey":"shift","style":{"fontFamily":"Open Sans"}},"credits":{"enabled":false},"exporting":{"enabled":false},"tooltip":{"crosshairs":true,"shared":true},"plotOptions":{"column":{"groupPadding":0.5,"pointWidth":5,"shadow":false,"borderWidth":0,"marker":{"enabled":true}},"area":{"marker":{"enabled":false},"states":{"hover":{"enabled":false}}},"line":{"lineWidth":0,"states":{"hover":{"enabled":false}}},"series":{"turboThreshold":4000,"animation":false}},"yAxis":{"title":{"text":"Units"}},"xAxis":{"type":"datetime","labels":{"rotation":45,"align":"left","overflow":"justify"}},"legend":{"layout":"vertical","align":"right","verticalAlign":"top","x":-10,"y":0,"borderWidth":0},"title":{"text":null}}'
